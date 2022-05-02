d7dba2d119aeb2c5c6ff554ca4c144305df0935c
Ahbab <ahbabshahin7788@gmail.com>
2022-05-02 17:54:55 +0600

    Routes & controller setup

--- app.js
+++ app.js
@@ -1,10 +1,23 @@
 const express = require('express');
 const app = express();
+const tasks = require('./routes/tasks');
 
-const port = 3000;
+//middleware
+app.use(express.static('./public'));
 
+app.use(express.json());
+
+//routes
 app.get('/hello', (req, res) => {
 	res.send('Task Manager');
 });
+
+app.get('/index.html', (req, res) => {
+	res.send();
+});
+
+app.use('/api/v1/tasks', tasks);
+
+const port = 3000;
 
 app.listen(port, console.log(`Server is listening on port ${port}`));
