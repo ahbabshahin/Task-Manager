619cbcd1c9e6151532693fa0f7277d43cfdda2b4
Ahbab <ahbabshahin7788@gmail.com>
2022-05-02 16:50:10 +0600

    Basic Express Server setup

--- app.js
+++ app.js
@@ -1,1 +1,10 @@
-console.log('Task Manager App')
+const express = require('express');
+const app = express();
+
+const port = 3000;
+
+app.get('/hello', (req, res) => {
+	res.send('Task Manager');
+});
+
+app.listen(port, console.log(`Server is listening on port ${port}`));
