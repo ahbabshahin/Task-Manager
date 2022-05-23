c090b15119e7d495abfbd714f3c34fca32ba66a5
Ahbab <ahbabshahin7788@gmail.com>
2022-05-23 15:56:37 +0600

    Update task done & null value insertion bug fixed

--- models/Task.js
+++ models/Task.js
@@ -2,7 +2,14 @@ const mongoose = require('mongoose');
 const TaskSchema = new mongoose.Schema({
 	name: {
 		type: String,
-		required: [true, 'must provide a name'],
+		required: [
+			true,
+			'must provide a name',
+			{
+				new: true,
+				runValidators: true,
+			},
+		],
 		trim: true,
 		maxlength: [20, 'name can not be more than 20 characters'],
 	},
